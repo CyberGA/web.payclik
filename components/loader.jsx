63a50f2a6980f865f06d7550f2603b4b60f7397b
CyberGA <etomu.joshua@gmail.com>
2023-01-24 21:41:52 +0000

    change color

--- components/loader.jsx
+++ components/loader.jsx
@@ -3,7 +3,7 @@ import { Loader } from "@mantine/core";
 const AppLoader = () => {
   return (
     <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
-      <Loader />
+      <Loader color="indigo" />
       <p className="mt-[20px] font-epilogue font-bold text-[20px] text-white text-center">
         Please wait...
       </p>
